f9af0b746e4f23d58e33a40adda786c941e59ed9
Haley Seymour <haleycseymour@comcast.net>
2022-04-12 23:45:07 -0700

    publish seattle upon opening

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -5,7 +5,6 @@ var storedArray = [];
 var buttonList = $('#buttonlist');
 var forecastCardDiv = $('#forecast')
 
-// ON PAGE LOAD, RETRIEVE LOCAL STORAGE AND LOAD
 function getLocalStorage() {
     var downloadedPlaces = JSON.parse(localStorage.getItem("places"));
     if (downloadedPlaces !== null) {
@@ -23,15 +22,19 @@ function getLocalStorage() {
 }
 getLocalStorage();
 
-// ON PAGE LOAD, LOAD SEATTLE WEATHER
-function pageLoad () {
-    city = 'Seattle';
-    var pageLoadURL = "https://api.openweathermap.org/data/2.5/weather?q=Seattle&units=imperial&appid=b7940b8b9a395337b92ee1766b299171";
-        fetch(pageLoadURL)
+$('#search').on("click", function (event) {
+    event.preventDefault();
+    city = $('#searchbox').val().trim();
+    city=city.charAt(0).toUpperCase() + city.slice(1);
+    function fetchLatLon() {
+        var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=b7940b8b9a395337b92ee1766b299171";
+    
+        fetch(requestURL)
             .then(function (response) {
                 if (!response.ok) {
                 alert('Please enter a valid city.');
                 throw response.json();
+                // empty name from input field
                 } 
             return response.json();
             })
@@ -66,7 +69,27 @@ function pageLoad () {
                             var spanIcon = $('<span>');
                             spanIcon.append(icon);
 
-                            postCurrentWeather(city, currentTemp, currentWind, currentHumidity, currentUVI, iconCode);
+                            var todaysWeather = $('#weatherToday')
+                            todaysWeather.children().eq(0).text(city + ' ' + date + ' ');
+                            todaysWeather.children().eq(0).css('font-weight', 'bold');
+                            todaysWeather.children().eq(1).text('Temp: ' + currentTemp);
+                            todaysWeather.children().eq(2).text('Wind: ' + currentWind);
+                            todaysWeather.children().eq(3).text('Humidity: ' + currentHumidity);
+
+                            var UVbox = $('<span>');
+                            if (currentUVI < 3) {
+                                UVbox.css("background", "green");
+                            } else if (3 <= currentUVI < 8) {
+                                UVbox.css("background", "yellow");
+                            } else {
+                                UVbox.css("background", "red");
+                            }
+                            UVbox.css("color", "white");
+                            UVbox.addClass("p-1");
+                            UVbox.text(currentUVI);
+                            todaysWeather.children().eq(4).text('UV Index: ');
+                            todaysWeather.children().eq(4).append(UVbox);
+                            todaysWeather.children().eq(0).append(spanIcon);
 
                             // 5 DAY FORECAST
                             var dailyForecast = data.daily;
@@ -85,78 +108,27 @@ function pageLoad () {
                                 var iconEl = $(`<img id="icon" alt="weather icon">`);
                                 iconEl.attr('src', forecastIconURL);
 
-                                createForecastCard(forecastedDate, forecastedTemp, forecastedWind, forecastedHumidity, iconEl)
+                                console.log(forecastedDate, forecastedTemp, forecastedWind, forecastedHumidity, forecastedIcon)
+
+                                var forecastCard = $('<div>');
+                                forecastCard.addClass("card col-md-2 text-white p-3");
+                                forecastCard.css('background-image', 'linear-gradient(to right, #2959c1, #261F6C)');
+                                var forecastDate = $('<h5>');
+                                forecastDate.text(forecastedDate);
+                                forecastDate.css('font-weight', 'bold');
+                                var forecastTemp = $('<p>');
+                                forecastTemp.text('Temp: ' + forecastedTemp);
+                                var forecastWind = $('<p>');
+                                forecastWind.text('Wind: ' + forecastedWind);
+                                var forecastHumidity = $('<p>');
+                                forecastHumidity.text('Humidity: ' + forecastedHumidity);
+                                forecastCard.append(forecastDate);
+                                forecastCard.append(iconEl)
+                                forecastCard.append(forecastTemp);
+                                forecastCard.append(forecastWind);
+                                forecastCard.append(forecastHumidity);
+                                forecastCardDiv.append(forecastCard);
                             }
-                        })
-                }
-                fetchForecast();
-            })
-}
-pageLoad();
-
-
-// WHEN SEARCH TEXT INPUT
-$('#search').on("click", function (event) {
-    event.preventDefault();
-    city = $('#searchbox').val().trim();
-    city=city.charAt(0).toUpperCase() + city.slice(1);
-    function fetchLatLon() {
-        var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=b7940b8b9a395337b92ee1766b299171";
-    
-        fetch(requestURL)
-            .then(function (response) {
-                if (!response.ok) {
-                alert('Please enter a valid city.');
-                throw response.json();
-                } 
-            return response.json();
-            })
-
-            .then(function(data) {
-                var latitude = data.coord.lat;
-                var longitude = data.coord.lon;
-
-                function fetchForecast() {
-
-                    var forecastRequest = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly,alerts&units=imperial&appid=" + APIKey;
-                
-                    fetch(forecastRequest)
-                        .then(function (response) {
-                            if (!response.ok) {
-                            throw response.json();
-                            } 
-                        return response.json();
-                        })
-                        .then(function(data) {
-                            // FOR TODAY
-                            var currentTemp = data.current.temp + "\xB0 F";
-                            var currentWind = data.current.wind_speed + " MPH";
-                            var currentHumidity = data.current.humidity + "%";
-                            var currentUVI = data.current.uvi;
-                            var iconCode = data.current.weather[0].icon;
-
-                            postCurrentWeather(city, currentTemp, currentWind, currentHumidity, currentUVI, iconCode)
-
-                            // 5 DAY FORECAST
-                            var dailyForecast = data.daily;
-                            forecastCardDiv.empty();
-                            for (let i = 1; i < 6; i++) {
-                                let currentDay = dailyForecast[i];
-                                var unix = currentDay.dt;
-                                var forecastedDate = moment.unix(unix).format('M/DD/YYYY');
-                                var forecastedTemp = currentDay.temp.day  + "\xB0 F";
-                                var forecastedWind = currentDay.wind_speed + " MPH";
-                                var forecastedHumidity = currentDay.humidity + " %";
-                                var forecastedIcon = currentDay.weather[0].icon;
-
-                                var forecastIconURL = 'https://openweathermap.org/img/wn/' + forecastedIcon + '@2x.png';
-
-                                var iconEl = $(`<img id="icon" alt="weather icon">`);
-                                iconEl.attr('src', forecastIconURL);
-
-                                createForecastCard(forecastedDate, forecastedTemp, forecastedWind, forecastedHumidity, iconEl);
-                            }
-                            // SEND TO LOCAL STORAGE
                             function setLocalStorage() {
                                 var storedPlaces = JSON.parse(localStorage.getItem("places"));
                                 if (storedPlaces !== null) {
@@ -167,7 +139,6 @@ $('#search').on("click", function (event) {
                             }
                             setLocalStorage();
                             
-                            // CREATE NEW BUTTON FOR CITY
                             var button = $('<button>');
                             button.addClass("col-12 mb-3 border-0 rounded p-1");
                             button.css('background', '#ADAEAE');
@@ -183,7 +154,8 @@ $('#search').on("click", function (event) {
     $('#searchbox').val('');    
 })
 
-// WHEN CITY BUTTON IS CLICKED
+
+
 buttonList.on("click", function (event) {
     var buttonID = event.target.id;
     city = buttonID; 
@@ -195,6 +167,7 @@ buttonList.on("click", function (event) {
                 if (!response.ok) {
                 alert('Please enter a valid city.');
                 throw response.json();
+                // empty name from input field
                 } 
             return response.json();
             })
@@ -229,7 +202,27 @@ buttonList.on("click", function (event) {
                             var spanIcon = $('<span>');
                             spanIcon.append(icon);
 
-                            postCurrentWeather(city, currentTemp, currentWind, currentHumidity, currentUVI, iconCode);
+                            var todaysWeather = $('#weatherToday')
+                            todaysWeather.children().eq(0).text(city + ' ' + date + ' ');
+                            todaysWeather.children().eq(0).css('font-weight', 'bold');
+                            todaysWeather.children().eq(1).text('Temp: ' + currentTemp);
+                            todaysWeather.children().eq(2).text('Wind: ' + currentWind);
+                            todaysWeather.children().eq(3).text('Humidity: ' + currentHumidity);
+
+                            var UVbox = $('<span>');
+                            if (currentUVI < 3) {
+                                UVbox.css("background", "green");
+                            } else if (3 <= currentUVI < 8) {
+                                UVbox.css("background", "yellow");
+                            } else {
+                                UVbox.css("background", "red");
+                            }
+                            UVbox.css("color", "white");
+                            UVbox.addClass("p-1");
+                            UVbox.text(currentUVI);
+                            todaysWeather.children().eq(4).text('UV Index: ');
+                            todaysWeather.children().eq(4).append(UVbox);
+                            todaysWeather.children().eq(0).append(spanIcon);
 
                             // 5 DAY FORECAST
                             var dailyForecast = data.daily;
@@ -248,7 +241,26 @@ buttonList.on("click", function (event) {
                                 var iconEl = $(`<img id="icon" alt="weather icon">`);
                                 iconEl.attr('src', forecastIconURL);
 
-                                createForecastCard(forecastedDate, forecastedTemp, forecastedWind, forecastedHumidity, iconEl)
+                                console.log(forecastedDate, forecastedTemp, forecastedWind, forecastedHumidity, forecastedIcon)
+
+                                var forecastCard = $('<div>');
+                                forecastCard.addClass("card col-md-2 text-white p-3");
+                                forecastCard.css('background-image', 'linear-gradient(to right, #2959c1, #261F6C)');
+                                var forecastDate = $('<h5>');
+                                forecastDate.text(forecastedDate);
+                                forecastDate.css('font-weight', 'bold');
+                                var forecastTemp = $('<p>');
+                                forecastTemp.text('Temp: ' + forecastedTemp);
+                                var forecastWind = $('<p>');
+                                forecastWind.text('Wind: ' + forecastedWind);
+                                var forecastHumidity = $('<p>');
+                                forecastHumidity.text('Humidity: ' + forecastedHumidity);
+                                forecastCard.append(forecastDate);
+                                forecastCard.append(iconEl)
+                                forecastCard.append(forecastTemp);
+                                forecastCard.append(forecastWind);
+                                forecastCard.append(forecastHumidity);
+                                forecastCardDiv.append(forecastCard);
                             }
                         })
                 }
@@ -257,54 +269,3 @@ buttonList.on("click", function (event) {
     }
     fetchLatLon()
 })    
-
-function postCurrentWeather(city, temp, wind, humidity, uvi, code) {
-    var iconURL = 'https://openweathermap.org/img/wn/' + code + '@2x.png';                       
-    var icon = $(`<img id="icon" alt="weather icon">`);
-    icon.attr('src', iconURL);
-    var spanIcon = $('<span>');
-    spanIcon.append(icon);
-    var todaysWeather = $('#weatherToday')
-    todaysWeather.children().eq(0).text(city + ' ' + date + ' ');
-    todaysWeather.children().eq(0).css('font-weight', 'bold');
-    todaysWeather.children().eq(1).text('Temp: ' + temp);
-    todaysWeather.children().eq(2).text('Wind: ' + wind);
-    todaysWeather.children().eq(3).text('Humidity: ' + humidity);
-
-    var UVbox = $('<span>');
-    if (uvi < 3) {
-        UVbox.css("background", "green");
-    } else if (3 <= uvi < 8) {
-        UVbox.css("background", "yellow");
-    } else {
-        UVbox.css("background", "red");
-    }
-    UVbox.css("color", "white");
-    UVbox.addClass("p-1");
-    UVbox.text(uvi);
-    todaysWeather.children().eq(4).text('UV Index: ');
-    todaysWeather.children().eq(4).append(UVbox);
-    todaysWeather.children().eq(0).append(spanIcon);
-}
-
-function createForecastCard (date, temp, wind, humidity, icon) {
-    var forecastCard = $('<div>');
-    forecastCard.addClass("card col-md-2 text-white p-3");
-    forecastCard.css('background-image', 'linear-gradient(to right, #2959c1, #261F6C)');
-    var forecastDate = $('<h5>');
-    forecastDate.text(date);
-    forecastDate.css('font-weight', 'bold');
-    var forecastTemp = $('<p>');
-    forecastTemp.text('Temp: ' + temp);
-    var forecastWind = $('<p>');
-    forecastWind.text('Wind: ' + wind);
-    var forecastHumidity = $('<p>');
-    forecastHumidity.text('Humidity: ' + humidity);
-    forecastCard.append(forecastDate);
-    forecastCard.append(icon)
-    forecastCard.append(forecastTemp);
-    forecastCard.append(forecastWind);
-    forecastCard.append(forecastHumidity);
-    forecastCardDiv.append(forecastCard);
-}
-
